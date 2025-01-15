5c852b88acead3a79ce789ec0a4de5dd72b8a915
installer <installer@gmail.com>
2025-01-15 15:39:09 -0500

    added parentnodes

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -40,3 +40,22 @@ class LeafNode(HtmlNode):
 
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+
+class ParentNode(HtmlNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props=props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("All parent nodes must have a tag")
+        if not self.children:
+            raise ValueError("All parent nodes must have children")
+        # Otherwise using recursion concentnate the HTML node and its children
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HtmlNode, LeafNode
+from htmlnode import HtmlNode, LeafNode, ParentNode
 
 
 class TestHtmlNode(unittest.TestCase):
@@ -26,6 +26,92 @@ class TestLeafNode(unittest.TestCase):
     def test_props_to_html_no_tag(self):
         node = LeafNode(None, "This is a paragraph")
         self.assertEqual(node.to_html(), "This is a paragraph")
+
+
+class TestParentNode(unittest.TestCase):
+    def test_to_html_with_props(self):
+        node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+        expected_output = (
+            "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>"
+        )
+        self.assertEqual(node.to_html(), expected_output)
+
+    # testing nested parent nodes
+    def test_to_html_nested_parent_nodes(self):
+        node = ParentNode(
+            "div",
+            [
+                LeafNode("b", "Bold text"),
+                ParentNode(
+                    "a",
+                    [
+                        LeafNode("span", "favorite social media"),
+                    ],
+                    {"href": "https://www.facebook.com"},
+                ),
+            ],
+        )
+        expected_output = '<div><b>Bold text</b><a href="https://www.facebook.com"><span>favorite social media</span></a></div>'
+        self.assertEqual(node.to_html(), expected_output)
+
+    # testing multiple levels of nesting:
+    def test_to_html_nested_parent_nodes_multiple_levels(self):
+        node = ParentNode(
+            "div",
+            [
+                LeafNode("b", "Bold text"),
+                ParentNode(
+                    "a",
+                    [
+                        LeafNode("span", "favorite social media"),
+                        ParentNode(
+                            "div",
+                            [
+                                LeafNode("p", "This is a paragraph"),
+                            ],
+                        ),
+                    ],
+                    {"href": "https://www.facebook.com"},
+                ),
+            ],
+        )
+        expected_output = '<div><b>Bold text</b><a href="https://www.facebook.com"><span>favorite social media</span><div><p>This is a paragraph</p></div></a></div>'
+        self.assertEqual(node.to_html(), expected_output)
+
+    # test invalid html: no tag
+    def test_to_html_no_tag(self):
+        node = ParentNode(None, [LeafNode("b", "Bold text")])
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+    # test invalid children: no children
+    def test_to_html_no_children(self):
+        node = ParentNode("div", [])
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+    # test multiple siblings
+    def test_to_html_multiple_siblings(self):
+        node = ParentNode(
+            "div",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode("i", "italic text"),
+                LeafNode("u", "underline text"),
+            ],
+        )
+        expected_output = (
+            "<div><b>Bold text</b><i>italic text</i><u>underline text</u></div>"
+        )
+        self.assertEqual(node.to_html(), expected_output)
 
 
 if __name__ == "__main__":
