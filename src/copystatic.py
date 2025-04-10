26b04267aaca32c5d2e352f424bc8e46ab9e129b
kw92 <kewilliamsdev@gmail.com>
2025-04-10 16:46:09 -0400

    Stop tracking public folder

---
+++ src/copystatic.py
@@ -0,0 +1,21 @@
+import os
+import shutil
+
+
+# This will copy contents from a source directory to a destination directory
+def copy_files_recursive(source_dir, destination_dir):
+    # if the path doesn't exist, then we should create it
+    if not os.path.exists(destination_dir):
+        print(f"Creating directory {destination_dir}...")
+        os.makedirs(destination_dir)
+
+    # We then iterate through the source directory and copy each file to destination
+    for filename in os.listdir(source_dir):
+        print(f"Processing {filename}...")
+        from_path = os.path.join(source_dir, filename)
+        to_path = os.path.join(destination_dir, filename)
+        print(f"Copying {from_path} to {to_path}..")
+        if os.path.isfile(from_path):
+            shutil.copy(from_path, to_path)
+        else:
+            copy_files_recursive(from_path, to_path)
--- src/main.py
+++ src/main.py
@@ -1,17 +1,28 @@
 from os import listdir, mkdir, path
 from shutil import copy, rmtree
 
+from copystatic import copy_files_recursive
 from htmlnode import LeafNode
 from textnode import TextNode, TextType
 
+dir_path_static = "./static"
+dir_path_public = "./public"
+
 
 def main():
-    textnode = TextNode("This is a text node", TextType.BOLD, "https://boot.dev")
-    print(textnode)
-    htmlnode = LeafNode("p", "This is a paragraph")
-    htmlnode2 = LeafNode("a", "This is a link", {"href": "https://boot.dev"})
-    print(htmlnode)
-    print(htmlnode2)
+    # textnode = TextNode("This is a text node", TextType.BOLD, "https://boot.dev")
+    # print(textnode)
+    # htmlnode = LeafNode("p", "This is a paragraph")
+    # htmlnode2 = LeafNode("a", "This is a link", {"href": "https://boot.dev"})
+    # print(htmlnode)
+    # print(htmlnode2)
+    if path.exists(dir_path_public):
+        print(f"Removing {dir_path_public}...")
+        rmtree(dir_path_public)
+
+    print(f"Copying {dir_path_static} to {dir_path_public}...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+    print("Done!")
 
 
 main()
